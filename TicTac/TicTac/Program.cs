a2831fb8a9f931e4d2e7fd0dfd15458789e0731b
sahiba <sahiba1214@gmail.com>
2021-12-04 09:39:01 +0530

    [sahiba1214] add : file added

---
+++ TicTac/TicTac/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe Game");
+            TicTac game = new TicTacGame();
+            game.gameBoard();
+        }
+    }
+}
---
+++ TicTac/TicTac/TicTacGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTac
+{
+    class TicTacGame
+    {
+        public void gameBoard()
+        {
+            char[] board = new char[10];
+
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    board[i] = ' ';
+                }
+            }
+        }
+    }
+}
